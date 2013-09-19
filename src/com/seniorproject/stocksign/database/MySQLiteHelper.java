8e99e61c27ae255f1552a37ab8aeee41488f4cd2
Sean <swilkinson09@gmail.com>
2013-09-18 21:56:36 -0400

    Added Stock Sign Launcher Icon

--- src/com/seniorproject/stocksign/database/MySQLiteHelper.java
+++ src/com/seniorproject/stocksign/database/MySQLiteHelper.java
@@ -34,7 +34,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase database) {
-		database.execSQL(DATABASE_CREATE);
+		//database.execSQL(DATABASE_CREATE);
 	}
 
 	@Override
