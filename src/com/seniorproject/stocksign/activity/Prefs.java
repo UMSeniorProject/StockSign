269ba07cddf32913a6e655c32d92d9e3da1436dd
Sean <swilkinson09@gmail.com>
2013-11-30 05:08:38 -0500

    Added "restart application" toast whenever user changed offline mode switch

--- src/com/seniorproject/stocksign/activity/Prefs.java
+++ src/com/seniorproject/stocksign/activity/Prefs.java
@@ -3,14 +3,19 @@ package com.seniorproject.stocksign.activity;
 import com.seniorproject.stocksign.R;
 import com.seniorproject.stocksign.debugging.Debugger;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class Prefs extends PreferenceActivity {
@@ -30,7 +35,8 @@ public class Prefs extends PreferenceActivity {
     public static class PrefsFragment extends PreferenceFragment
     {
     	
-    	
+        
+    
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
@@ -38,6 +44,17 @@ public class Prefs extends PreferenceActivity {
             addPreferencesFromResource(R.xml.prefs);
             
         }
+
+		@Override
+		public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+				Preference preference) {
+			// TODO Auto-generated method stub
+			Toast.makeText(getActivity(),  "Settings will take affect after restarting Stock Sign" , Toast.LENGTH_SHORT).show();
+
+			return super.onPreferenceTreeClick(preferenceScreen, preference);
+		}
+		
+
        
     }
     
