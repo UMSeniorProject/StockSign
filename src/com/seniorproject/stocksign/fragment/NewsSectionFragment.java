762e38731bde9526cc023ace48b015eefacf4c20
Sean <swilkinson09@gmail.com>
2013-12-04 16:37:43 -0500

    News Page now displays news

--- src/com/seniorproject/stocksign/fragment/NewsSectionFragment.java
+++ src/com/seniorproject/stocksign/fragment/NewsSectionFragment.java
@@ -3,22 +3,22 @@
  */
 package com.seniorproject.stocksign.fragment;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,74 +28,160 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.seniorproject.stocksign.R;
-import com.seniorproject.stocksign.activity.MainActivity;
-import com.seniorproject.stocksign.debugging.Debugger;
 
 /**
  * @author Sean
- *
+ * 
  */
-public class NewsSectionFragment extends ListFragment  {
-	
-	
+public class NewsSectionFragment extends ListFragment {
 
-	static ArrayList headlines,h;
-	static ArrayList links,l;
-	static XmlPullParser xpp;
+	private ArrayList<RSSItem> itemlist = null;
+	private RSSListAdaptor rssadaptor = null;
 
-	/**Should not be instantiated, empty constructor */
+	/** Should not be instantiated, empty constructor */
 	public NewsSectionFragment() {
-		
-		
-		
+
 	}
 
 	/**
 	 * Called when fragment is starting, where most inistialization should go
-	 * @param 
+	 * 
+	 * @param
 	 * @return rootView The view to be displayed
 	 * */
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
-		View rootView = inflater.inflate(R.layout.fragment_news,
-				container, false);
 
-		
+		View rootView = inflater.inflate(R.layout.fragment_news, container,
+				false);
 
-	    // Initializing instance variables
-	    // Initializing instance variables
-	    headlines = new ArrayList();
-	    links = new ArrayList();
+		itemlist = new ArrayList<RSSItem>();
 
-	    h = new ArrayList();
-	    l = new ArrayList();
-	    
-	    // Binding data
-	    ArrayAdapter adapter = new ArrayAdapter(this.getActivity(),
-	            android.R.layout.simple_list_item_1, h);
-
-	    setListAdapter(adapter);
-
+		new RetrieveRSSFeeds().execute();
 
 		return rootView;
 	}
-	
-
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		// TODO Auto-generated method stub
 		super.onListItemClick(l, v, position, id);
-		
-		   Uri uri = Uri.parse((String) links.get(position));
-		   Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-		   startActivity(intent);
+
+		RSSItem data = itemlist.get(position);
+
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data.link));
+
+		startActivity(intent);
 	}
 
+	private void retrieveRSSFeed(String urlToRssFeed, ArrayList<RSSItem> list) {
+		try {
+			URL url = new URL(urlToRssFeed);
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser parser = factory.newSAXParser();
+			XMLReader xmlreader = parser.getXMLReader();
+			RSSParser theRssHandler = new RSSParser(list);
 
-	
+			xmlreader.setContentHandler(theRssHandler);
 
-	
+			InputSource is = new InputSource(url.openStream());
 
+			xmlreader.parse(is);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class RetrieveRSSFeeds extends AsyncTask<Void, Void, Void> {
+		private ProgressDialog progress = null;
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			retrieveRSSFeed(
+					"http://feeds.marketwatch.com/marketwatch/topstories",
+					itemlist);
+
+			rssadaptor = new RSSListAdaptor(getActivity(),
+					R.layout.rssitemview, itemlist);
+
+			return null;
+		}
+
+		@Override
+		protected void onCancelled() {
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			progress = ProgressDialog.show(getActivity(), null,
+					"Loading News Feed...");
+
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			setListAdapter(rssadaptor);
+
+			progress.dismiss();
+
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			super.onProgressUpdate(values);
+		}
+	}
+
+	private class RSSListAdaptor extends ArrayAdapter<RSSItem> {
+		private List<RSSItem> objects = null;
+
+		public RSSListAdaptor(Context context, int textviewid,
+				List<RSSItem> objects) {
+			super(context, textviewid, objects);
+
+			this.objects = objects;
+		}
+
+		@Override
+		public int getCount() {
+			return ((null != objects) ? objects.size() : 0);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public RSSItem getItem(int position) {
+			return ((null != objects) ? objects.get(position) : null);
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+
+			if (null == view) {
+				LayoutInflater vi = (LayoutInflater) getActivity()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = vi.inflate(R.layout.rssitemview, null);
+			}
+
+			RSSItem data = objects.get(position);
+
+			if (null != data) {
+				TextView title = (TextView) view.findViewById(R.id.txtTitle);
+				TextView date = (TextView) view.findViewById(R.id.txtDate);
+				TextView description = (TextView) view
+						.findViewById(R.id.txtDescription);
+
+				title.setText(data.title);
+				date.setText("on " + data.date);
+				description.setText(data.description);
+			}
+
+			return view;
+		}
+	}
 }
---
+++ src/com/seniorproject/stocksign/fragment/RSSItem.java
@@ -0,0 +1,8 @@
+package com.seniorproject.stocksign.fragment;
+
+public class RSSItem {
+	public String title;
+	public String date;
+	public String link;
+	public String description;
+}
