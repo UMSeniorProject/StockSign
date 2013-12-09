28f3af8d80a9b0380a2e0524a5d382936ab15b96
Sean <swilkinson09@gmail.com>
2013-12-09 00:59:01 -0500

    Created Top 50 stocks page for each category.

--- src/com/seniorproject/stocksign/display/DisplayStockRatioData.java
+++ src/com/seniorproject/stocksign/display/DisplayStockRatioData.java
@@ -1,22 +1,17 @@
 package com.seniorproject.stocksign.display;
 
-import com.seniorproject.stocksign.R;
-import com.seniorproject.stocksign.activity.ActivityConstants;
-import com.seniorproject.stocksign.database.RatioConstants;
-import com.seniorproject.stocksign.database.Stock;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TextView;
 import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
 
-import com.google.gson.Gson;
+import com.seniorproject.stocksign.R;
 
-public class DisplayStockRatioData extends Activity{
+public class DisplayStockRatioData extends Activity {
 
 	TextView stockName;
 	TextView stockTicker;
@@ -24,144 +19,142 @@ public class DisplayStockRatioData extends Activity{
 	TextView stockSector;
 	TextView stockIndustry;
 	TableLayout ratioTable;
-	//Stock currentStock;
+	// Stock currentStock;
 	String[] ratioDataArray;
 	String[] ratioDataNames;
 	int callingActivityID;
-	
+
 	public void initialize(Intent i) {
 		stockName = (TextView) findViewById(R.id.tvStockname);
 		stockTicker = (TextView) findViewById(R.id.tvStockticker);
 		stockCountry = (TextView) findViewById(R.id.tvStockCountryValue);
 		stockSector = (TextView) findViewById(R.id.tvStockSectorValue);
 		stockIndustry = (TextView) findViewById(R.id.tvStockIndustryValue);
-		ratioTable = (TableLayout) findViewById(R.id.tlStockratios); 
-		//get data
+		ratioTable = (TableLayout) findViewById(R.id.tlStockratios);
+		// get data
 		Bundle b = i.getBundleExtra("ratioData");
 		ratioDataArray = b.getStringArray("values");
 		ratioDataNames = b.getStringArray("keySet");
-		callingActivityID = i.getIntExtra("activityID", 0);	
+		callingActivityID = i.getIntExtra("activityID", 0);
 	}
-	
-	/*public void createRatioDataArray(Stock s) {
-		ratioDataArray = new String[RatioConstants.totalRatios];
-		ratioDataArray[RatioConstants.ticker] = s.getTicker();
-		ratioDataArray[RatioConstants.company] = s.getCompany();
-		ratioDataArray[RatioConstants.sector] = s.getSector();
-		ratioDataArray[RatioConstants.industry] = s.getIndustry();
-		ratioDataArray[RatioConstants.country] = s.getCountry();
-		ratioDataArray[RatioConstants.PE] = s.getPe();
-		ratioDataArray[RatioConstants.FPE] = s.getForward_pe();
-		ratioDataArray[RatioConstants.PEG] = s.getPeg();
-		ratioDataArray[RatioConstants.PS] = s.getPs();
-		ratioDataArray[RatioConstants.PB] = s.getPb();
-		ratioDataArray[RatioConstants.PC] = s.getPc();
-		ratioDataArray[RatioConstants.PFCF] = s.getPriceFreeCashFlow();
-		ratioDataArray[RatioConstants.dYield] = s.getDividendYield();
-		ratioDataArray[RatioConstants.PO] = s.getPayoutRatio();
-		ratioDataArray[RatioConstants.EPSTHISYR] = s.getEpsgThisYear();
-		ratioDataArray[RatioConstants.EPSNEXTYEAR] = "Missing";
-		ratioDataArray[RatioConstants.EPS5] = s.getEpsgPast5Years();
-		ratioDataArray[RatioConstants.EG] = s.getEpsgNext5Years();
-		ratioDataArray[RatioConstants.SALES5] = s.getSalesgPast5Years();
-		ratioDataArray[RatioConstants.FLOAT] = "Missing";
-		ratioDataArray[RatioConstants.INSIOWN] = s.getInsiderOwnership();
-		ratioDataArray[RatioConstants.INSITRANS] = "Missing";
-		ratioDataArray[RatioConstants.INSTOWN] = "Missing";
-		ratioDataArray[RatioConstants.INSTTRANS] = s.getInstitutionalTransactions();
-		ratioDataArray[RatioConstants.SHORT] = s.getFloatShort();
-		ratioDataArray[RatioConstants.ROA] = s.getReturnOnAssets();
-		ratioDataArray[RatioConstants.ROE] = s.getReturnOnEquity();
-		ratioDataArray[RatioConstants.CR] = s.getCurrentRatio();
-		ratioDataArray[RatioConstants.QR] = s.getQuickRatio();
-		ratioDataArray[RatioConstants.LTDE] = s.getLtDebtEquity();
-		ratioDataArray[RatioConstants.PM] = "Missing";
-		ratioDataArray[RatioConstants.BETA] = "Missing";
-		ratioDataArray[RatioConstants.WVOL] = "Missing";
-		ratioDataArray[RatioConstants.WVOL] = "Missing";
-		ratioDataArray[RatioConstants.RSI] = s.getRsi();
-		ratioDataArray[RatioConstants.AVGVOL] = "Missing";
-		ratioDataArray[RatioConstants.RVOL] = "Missing";
-		ratioDataArray[RatioConstants.TOTALSCORE] = "Missing";
-		ratioDataArray[RatioConstants.DIVSCORE] = "Missing";
-		ratioDataArray[RatioConstants.GROWTHSCORE] = "Missing";
-	}*/
-	
+
+	/*
+	 * public void createRatioDataArray(Stock s) { ratioDataArray = new
+	 * String[RatioConstants.totalRatios]; ratioDataArray[RatioConstants.ticker]
+	 * = s.getTicker(); ratioDataArray[RatioConstants.company] = s.getCompany();
+	 * ratioDataArray[RatioConstants.sector] = s.getSector();
+	 * ratioDataArray[RatioConstants.industry] = s.getIndustry();
+	 * ratioDataArray[RatioConstants.country] = s.getCountry();
+	 * ratioDataArray[RatioConstants.PE] = s.getPe();
+	 * ratioDataArray[RatioConstants.FPE] = s.getForward_pe();
+	 * ratioDataArray[RatioConstants.PEG] = s.getPeg();
+	 * ratioDataArray[RatioConstants.PS] = s.getPs();
+	 * ratioDataArray[RatioConstants.PB] = s.getPb();
+	 * ratioDataArray[RatioConstants.PC] = s.getPc();
+	 * ratioDataArray[RatioConstants.PFCF] = s.getPriceFreeCashFlow();
+	 * ratioDataArray[RatioConstants.dYield] = s.getDividendYield();
+	 * ratioDataArray[RatioConstants.PO] = s.getPayoutRatio();
+	 * ratioDataArray[RatioConstants.EPSTHISYR] = s.getEpsgThisYear();
+	 * ratioDataArray[RatioConstants.EPSNEXTYEAR] = "Missing";
+	 * ratioDataArray[RatioConstants.EPS5] = s.getEpsgPast5Years();
+	 * ratioDataArray[RatioConstants.EG] = s.getEpsgNext5Years();
+	 * ratioDataArray[RatioConstants.SALES5] = s.getSalesgPast5Years();
+	 * ratioDataArray[RatioConstants.FLOAT] = "Missing";
+	 * ratioDataArray[RatioConstants.INSIOWN] = s.getInsiderOwnership();
+	 * ratioDataArray[RatioConstants.INSITRANS] = "Missing";
+	 * ratioDataArray[RatioConstants.INSTOWN] = "Missing";
+	 * ratioDataArray[RatioConstants.INSTTRANS] =
+	 * s.getInstitutionalTransactions(); ratioDataArray[RatioConstants.SHORT] =
+	 * s.getFloatShort(); ratioDataArray[RatioConstants.ROA] =
+	 * s.getReturnOnAssets(); ratioDataArray[RatioConstants.ROE] =
+	 * s.getReturnOnEquity(); ratioDataArray[RatioConstants.CR] =
+	 * s.getCurrentRatio(); ratioDataArray[RatioConstants.QR] =
+	 * s.getQuickRatio(); ratioDataArray[RatioConstants.LTDE] =
+	 * s.getLtDebtEquity(); ratioDataArray[RatioConstants.PM] = "Missing";
+	 * ratioDataArray[RatioConstants.BETA] = "Missing";
+	 * ratioDataArray[RatioConstants.WVOL] = "Missing";
+	 * ratioDataArray[RatioConstants.WVOL] = "Missing";
+	 * ratioDataArray[RatioConstants.RSI] = s.getRsi();
+	 * ratioDataArray[RatioConstants.AVGVOL] = "Missing";
+	 * ratioDataArray[RatioConstants.RVOL] = "Missing";
+	 * ratioDataArray[RatioConstants.TOTALSCORE] = "Missing";
+	 * ratioDataArray[RatioConstants.DIVSCORE] = "Missing";
+	 * ratioDataArray[RatioConstants.GROWTHSCORE] = "Missing"; }
+	 */
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.displaystockdata);
 		initialize(getIntent());
-		//I can also get the calling activity ID but its unnecessary at this time
+		// I can also get the calling activity ID but its unnecessary at this
+		// time
 		displayRatioData();
 	}
-	
+
 	public void displayRatioData() {
-		
+
 		stockName.setText(ratioDataArray[0].substring(1));
 		stockTicker.setText(ratioDataArray[36]);
 		stockCountry.setText(ratioDataArray[1]);
 		stockSector.setText(ratioDataArray[34]);
 		stockIndustry.setText(ratioDataArray[12]);
-		
+
 		int[] alternatingRGBColor = new int[3];
 		int alternator = 0;
-		
-		//36 needs to be changed to a variable as soon as we update Kinvey
-		
-        // Go through each item in the array
-        for (int current = 2; current < 36; current++)
-        {       	
-        	//display all ratios besides Sector and Industry
-        	if(current!=12 && current!=34) {
-        		
-            	if(alternator==0) {
-            		alternatingRGBColor[0] = 209;//red
-            		alternatingRGBColor[1] = 209;//green
-            		alternatingRGBColor[2] = 209;//blue
-            		alternator = 1;
-            	}
-            	else {
-            		alternatingRGBColor[0] = 232;//red
-            		alternatingRGBColor[1] = 232;//green
-            		alternatingRGBColor[2] = 232;//blue
-            		alternator = 0;
-            	}
-        		
-	            // Create a TableRow and give it an ID
-	            TableRow tr = new TableRow(this);
-	            tr.setId(100+current);
-	            tr.setBackgroundColor(Color.rgb(alternatingRGBColor[0], alternatingRGBColor[1], alternatingRGBColor[2]));
-	            tr.setLayoutParams(new LayoutParams(
-	                    LayoutParams.WRAP_CONTENT));   
-	
-	            // Create a TextView to hold the label of the ratio
-	            TextView labelTV = new TextView(this);
-	            labelTV.setId(200+current);
-	            labelTV.setText(ratioDataNames[current]);
-	            labelTV.setTextColor(Color.BLACK);
-	            labelTV.setTextSize(15);
-	            labelTV.setLayoutParams(new LayoutParams(
-	                    LayoutParams.WRAP_CONTENT));
-	            tr.addView(labelTV);
-	
-	            // Create a TextView to hold the value of the ratio
-	            TextView valueTV = new TextView(this);
-	            valueTV.setId(300+current);
-	            valueTV.setText(ratioDataArray[current]);
-	            valueTV.setTextColor(Color.BLACK);
-	            valueTV.setTextSize(15);
-	            valueTV.setLayoutParams(new LayoutParams(
-	                    LayoutParams.WRAP_CONTENT));
-	            tr.addView(valueTV);
-	
-	            // Add the TableRow to the TableLayout
-	            ratioTable.addView(tr, new TableLayout.LayoutParams(
-	                    LayoutParams.MATCH_PARENT,
-	                    LayoutParams.WRAP_CONTENT));
-        	}
-        }
+
+		// 36 needs to be changed to a variable as soon as we update Kinvey
+
+		// Go through each item in the array
+		for (int current = 2; current < 36; current++) {
+			// display all ratios besides Sector and Industry
+			if (current != 12 && current != 34) {
+
+				if (alternator == 0) {
+					alternatingRGBColor[0] = 209;// red
+					alternatingRGBColor[1] = 209;// green
+					alternatingRGBColor[2] = 209;// blue
+					alternator = 1;
+				} else {
+					alternatingRGBColor[0] = 232;// red
+					alternatingRGBColor[1] = 232;// green
+					alternatingRGBColor[2] = 232;// blue
+					alternator = 0;
+				}
+
+				// Create a TableRow and give it an ID
+				TableRow tr = new TableRow(this);
+				tr.setId(100 + current);
+				tr.setBackgroundColor(Color.rgb(alternatingRGBColor[0],
+						alternatingRGBColor[1], alternatingRGBColor[2]));
+				tr.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT));
+
+				// Create a TextView to hold the label of the ratio
+				TextView labelTV = new TextView(this);
+				labelTV.setId(200 + current);
+				labelTV.setText(ratioDataNames[current]);
+				labelTV.setTextColor(Color.BLACK);
+				labelTV.setTextSize(15);
+				labelTV.setLayoutParams(new LayoutParams(
+						LayoutParams.WRAP_CONTENT));
+				tr.addView(labelTV);
+
+				// Create a TextView to hold the value of the ratio
+				TextView valueTV = new TextView(this);
+				valueTV.setId(300 + current);
+				valueTV.setText(ratioDataArray[current]);
+				valueTV.setTextColor(Color.BLACK);
+				valueTV.setTextSize(15);
+				valueTV.setLayoutParams(new LayoutParams(
+						LayoutParams.WRAP_CONTENT));
+				tr.addView(valueTV);
+
+				// Add the TableRow to the TableLayout
+				ratioTable.addView(tr, new TableLayout.LayoutParams(
+						LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+			}
+		}
 	}
 
 }
--- src/com/seniorproject/stocksign/display/GrowthScore.java
+++ src/com/seniorproject/stocksign/display/GrowthScore.java
@@ -1,11 +1,35 @@
 package com.seniorproject.stocksign.display;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
 
+import com.kinvey.android.AsyncAppData;
+import com.kinvey.android.Client;
+import com.kinvey.android.callback.KinveyListCallback;
+import com.kinvey.java.Query;
 import com.seniorproject.stocksign.R;
+import com.seniorproject.stocksign.activity.ActivityConstants;
+import com.seniorproject.stocksign.database.Stock;
+import com.seniorproject.stocksign.kinveyconnection.KinveyConnectionSingleton;
 
 public class GrowthScore extends Activity {
+
+	Client mKinveyClient = null;
+	String StockDataTableName = "StockRatioDataTable";
+
+	static Stock[] stocks = null;
+	int maxNamesDisplayed = 50;
+	private TableLayout ratioTable;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +37,149 @@ public class GrowthScore extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.growthscore);
+
+		mKinveyClient = KinveyConnectionSingleton.getKinveyClient();
+		initialize(getIntent());
+		kinveyDataFetcher();
+
+	}
+
+	private void kinveyDataFetcher() {
+		// TODO Auto-generated method stub
+		Query fetchRank = mKinveyClient.query();
+		Query fetchRequest = new Query(); // mKinveyClient.query();
+
+		// QUERY MUST BE CHANGED
+		// fetchRank.startsWith("PE", "9");
+
+		fetchRequest.startsWith("Ticker", "G");
+
+		fetchRequest.setLimit(maxNamesDisplayed);
+
+		kinveyFetchQuery(fetchRequest);
+	}
+
+	// method to fetch specific Query from Kinvey
+	private void kinveyFetchQuery(Query fetchQuery) {
+		AsyncAppData<Stock> myData = mKinveyClient.appData(StockDataTableName,
+				Stock.class);
+		myData.get(fetchQuery, new KinveyListCallback<Stock>() {
+			@Override
+			public void onSuccess(Stock[] arg0) {
+				// TODO Auto-generated method stub
+				Log.i("success", "got: " + arg0.toString());
+				if (arg0.length == 0) {
+					stocks = null;
+					System.out.println("STOCKS IS: NULL");
+				} else {
+					stocks = arg0;
+					int[] alternatingRGBColor = new int[3];
+					int alternator = 0;
+
+					// Go through each item in the array
+					for (int current = 0; current < maxNamesDisplayed; current++) {
+						// display all ratios besides Sector and Industry
+
+						if (alternator == 0) {
+							alternatingRGBColor[0] = 209;// red
+							alternatingRGBColor[1] = 209;// green
+							alternatingRGBColor[2] = 209;// blue
+							alternator = 1;
+						} else {
+							alternatingRGBColor[0] = 232;// red
+							alternatingRGBColor[1] = 232;// green
+							alternatingRGBColor[2] = 232;// blue
+							alternator = 0;
+						}
+
+						// Create a TableRow and give it an ID
+						TableRow tr = new TableRow(GrowthScore.this);
+						tr.setId(100 + current);
+						tr.setBackgroundColor(Color.rgb(alternatingRGBColor[0],
+								alternatingRGBColor[1], alternatingRGBColor[2]));
+						tr.setLayoutParams(new LayoutParams(
+								LayoutParams.WRAP_CONTENT));
+
+						// Create a TextView to hold the label of the ratio
+						TextView labelTV = new TextView(GrowthScore.this);
+						labelTV.setId(200 + current);
+						labelTV.setText(stocks[current].getTicker());
+						labelTV.setTextColor(Color.BLACK);
+						labelTV.setTextSize(17);
+						labelTV.setLayoutParams(new LayoutParams(
+								LayoutParams.WRAP_CONTENT));
+						labelTV.setGravity(Gravity.CENTER);
+						tr.addView(labelTV);
+
+						// Create a TextView to hold the value of the ratio
+						TextView valueTV = new TextView(GrowthScore.this);
+						valueTV.setId(300 + current);
+						valueTV.setText(stocks[current].getRsi());
+						valueTV.setTextColor(Color.BLACK);
+						valueTV.setTextSize(17);
+						valueTV.setLayoutParams(new LayoutParams(
+								LayoutParams.WRAP_CONTENT));
+						valueTV.setGravity(Gravity.CENTER);
+						tr.addView(valueTV);
+
+						// Add the TableRow to the TableLayout
+						ratioTable.addView(tr, new TableLayout.LayoutParams(
+								LayoutParams.MATCH_PARENT,
+								LayoutParams.WRAP_CONTENT));
+						addClick(tr, stocks[current]);
+
+					}
+					// displayAutoComplete();
+					System.out.println("STOCKS IS: " + stocks[0].getCompany());
+
+				}
+			}
+
+			private void addClick(TableRow tr, final Stock stock) {
+				// TODO Auto-generated method stub
+				tr.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+
+						String[] values = stock.values().toString().split(",");
+						String[] keySet = stock.keySet().toString().split(",");
+
+						Intent i = new Intent(GrowthScore.this,
+								DisplayStockRatioData.class);
+						Bundle b = new Bundle();
+						b.putStringArray("values", values);
+						b.putStringArray("keySet", keySet);
+						i.putExtra("ratioData", b);
+						i.putExtra("activityID",
+								ActivityConstants.SearchStockActivity); // send
+																		// the
+																		// activity
+																		// id
+						startActivity(i);
+
+					}
+
+				});
+			}
+
+			@Override
+			public void onFailure(Throwable error) {
+				Log.d("fail", "failed to fetchByFilterCriteria: "
+						+ error.getCause().getMessage());
+			}
+
+		});
+
+	}
+
+	private void initialize(Intent i) {
+		// TODO Auto-generated method stub
+		TextView hotTextView = (TextView) findViewById(R.id.section_label);
+		hotTextView.setText("Growth Score");
+
+		ratioTable = (TableLayout) findViewById(R.id.tlGrowthScores);
+
 	}
 }
